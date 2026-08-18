ae4768cdd8b7800e80ea15997bb69dd2b31920ba
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:52:07 +0530

    phase3: wire persuasive commercial journey

--- app/performance-marketing-agency-jaipur/page.tsx
+++ app/performance-marketing-agency-jaipur/page.tsx
@@ -1,6 +1,7 @@
-import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
+import PersuasiveCommercialLandingPage from '@/components/commercial/PersuasiveCommercialLandingPage'
+import { safeCommercialContent } from '@/components/commercial/safeCommercialContent'
 import { JAIPUR_CONTENT } from './_data/content'
 
 export default function JaipurPerformanceMarketingPage() {
-  return <CommercialLandingPage content={JAIPUR_CONTENT} />
+  return <PersuasiveCommercialLandingPage content={safeCommercialContent(JAIPUR_CONTENT)} />
 }
