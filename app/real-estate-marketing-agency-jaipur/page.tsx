cf220c36f55628d4db9e96a269a57bc09f4dbe39
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:53:43 +0530

    phase3: wire persuasive commercial journey

--- app/real-estate-marketing-agency-jaipur/page.tsx
+++ app/real-estate-marketing-agency-jaipur/page.tsx
@@ -1,6 +1,7 @@
-import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
+import PersuasiveCommercialLandingPage from '@/components/commercial/PersuasiveCommercialLandingPage'
+import { safeCommercialContent } from '@/components/commercial/safeCommercialContent'
 import { JAIPUR_REAL_ESTATE_CONTENT } from './_data/content'
 
 export default function JaipurRealEstatePage() {
-  return <CommercialLandingPage content={JAIPUR_REAL_ESTATE_CONTENT} />
+  return <PersuasiveCommercialLandingPage content={safeCommercialContent(JAIPUR_REAL_ESTATE_CONTENT)} />
 }
