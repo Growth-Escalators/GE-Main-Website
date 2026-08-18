d5084355481922337b6f136cefde219c0691b569
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:28:05 +0530

    phase3: migrate b2b page to commercial system

--- app/b2b-lead-generation-agency/page.tsx
+++ app/b2b-lead-generation-agency/page.tsx
@@ -1,6 +1,6 @@
-import IndustryLandingPage from '@/components/landing/IndustryLandingPage'
+import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
 import { B2B_LEAD_GEN_CONTENT } from './_data/content'
 
 export default function B2BLeadGenerationAgencyPage() {
-  return <IndustryLandingPage content={B2B_LEAD_GEN_CONTENT} />
+  return <CommercialLandingPage content={B2B_LEAD_GEN_CONTENT} />
 }
