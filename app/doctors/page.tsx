313f8989543abdb26d7913252e6c42b407105227
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:52:53 +0530

    phase3: wire persuasive commercial journey

--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -1,6 +1,7 @@
-import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
+import PersuasiveCommercialLandingPage from '@/components/commercial/PersuasiveCommercialLandingPage'
+import { safeCommercialContent } from '@/components/commercial/safeCommercialContent'
 import { DOCTORS_CONTENT } from './_data/content'
 
 export default function DoctorsPage() {
-  return <CommercialLandingPage content={DOCTORS_CONTENT} />
+  return <PersuasiveCommercialLandingPage content={safeCommercialContent(DOCTORS_CONTENT)} />
 }
