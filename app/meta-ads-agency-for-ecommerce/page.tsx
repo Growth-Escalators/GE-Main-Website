2f8ee1b9804f71d963d0aa0b7b277102e17d7ff9
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:53:18 +0530

    phase3: wire persuasive commercial journey

--- app/meta-ads-agency-for-ecommerce/page.tsx
+++ app/meta-ads-agency-for-ecommerce/page.tsx
@@ -1,6 +1,7 @@
-import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
+import PersuasiveCommercialLandingPage from '@/components/commercial/PersuasiveCommercialLandingPage'
+import { safeCommercialContent } from '@/components/commercial/safeCommercialContent'
 import { META_ADS_ECOMMERCE_CONTENT } from './_data/content'
 
 export default function MetaAdsEcommercePage() {
-  return <CommercialLandingPage content={META_ADS_ECOMMERCE_CONTENT} />
+  return <PersuasiveCommercialLandingPage content={safeCommercialContent(META_ADS_ECOMMERCE_CONTENT)} />
 }
