0cbed2033cf4899281c1b574227d23af0ebd575b
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:27:23 +0530

    phase3: migrate ecommerce page to commercial system

--- app/ecommerce-advertising-agency/page.tsx
+++ app/ecommerce-advertising-agency/page.tsx
@@ -1,6 +1,6 @@
-import IndustryLandingPage from '@/components/landing/IndustryLandingPage'
+import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
 import { ECOMMERCE_ADVERTISING_AGENCY_CONTENT } from './_data/content'
 
 export default function EcommerceAdvertisingAgencyPage() {
-  return <IndustryLandingPage content={ECOMMERCE_ADVERTISING_AGENCY_CONTENT} />
+  return <CommercialLandingPage content={ECOMMERCE_ADVERTISING_AGENCY_CONTENT} />
 }
