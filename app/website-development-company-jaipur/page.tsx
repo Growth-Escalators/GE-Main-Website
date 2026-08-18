6da371c36da4c9c0a09a3cab9e474ec2ea6b2a21
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:27:31 +0530

    phase3: migrate website development page

--- app/website-development-company-jaipur/page.tsx
+++ app/website-development-company-jaipur/page.tsx
@@ -1,6 +1,6 @@
-import IndustryLandingPage from '@/components/landing/IndustryLandingPage'
+import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
 import { WEBSITE_JAIPUR_CONTENT } from './_data/content'
 
 export default function WebsiteDevelopmentJaipurPage() {
-  return <IndustryLandingPage content={WEBSITE_JAIPUR_CONTENT} />
+  return <CommercialLandingPage content={WEBSITE_JAIPUR_CONTENT} />
 }
