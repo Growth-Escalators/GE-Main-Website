ce5cd67cb49e57df831b5e676650ee19aa604f6d
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:31:21 +0530

    phase3: migrate ecommerce Meta Ads spoke

--- app/meta-ads-agency-for-ecommerce/page.tsx
+++ app/meta-ads-agency-for-ecommerce/page.tsx
@@ -1,6 +1,6 @@
-import IndustryLandingPage from '@/components/landing/IndustryLandingPage'
+import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
 import { META_ADS_ECOMMERCE_CONTENT } from './_data/content'
 
 export default function MetaAdsEcommercePage() {
-  return <IndustryLandingPage content={META_ADS_ECOMMERCE_CONTENT} />
+  return <CommercialLandingPage content={META_ADS_ECOMMERCE_CONTENT} />
 }
