0a5b0c58ff011ea274a9be35726d080b413bbcb7
Jatin-ge <Jatin@growthescalators.com>
2026-08-18 23:53:11 +0530

    phase3: wire persuasive commercial journey

--- app/b2b-lead-generation-agency/page.tsx
+++ app/b2b-lead-generation-agency/page.tsx
@@ -1,6 +1,7 @@
-import CommercialLandingPage from '@/components/commercial/CommercialLandingPage'
+import PersuasiveCommercialLandingPage from '@/components/commercial/PersuasiveCommercialLandingPage'
+import { safeCommercialContent } from '@/components/commercial/safeCommercialContent'
 import { B2B_LEAD_GEN_CONTENT } from './_data/content'
 
 export default function B2BLeadGenerationAgencyPage() {
-  return <CommercialLandingPage content={B2B_LEAD_GEN_CONTENT} />
+  return <PersuasiveCommercialLandingPage content={safeCommercialContent(B2B_LEAD_GEN_CONTENT)} />
 }
