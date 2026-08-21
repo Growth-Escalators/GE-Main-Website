0042abc05ff1bd7cf9480970bedb03b419cb78fa
Jatin-ge <Jatin@growthescalators.com>
2026-08-21 16:37:26 +0530

    Add women's clothing performance page

---
+++ app/d2c/fashion/womens-clothing/page.tsx
@@ -0,0 +1,3 @@
+import SeoClusterLanding, { getSeoClusterMetadata } from '@/components/seo/SeoClusterLanding'
+export const metadata = getSeoClusterMetadata('womensClothing')
+export default function Page(){ return <SeoClusterLanding pageKey="womensClothing" /> }
