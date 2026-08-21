458d1a957bb6e51295bf8d3a9e6ddec77bdf7df1
Jatin-ge <Jatin@growthescalators.com>
2026-08-21 16:37:12 +0530

    Add custom software Jaipur page

---
+++ app/custom-software-development-company-jaipur/page.tsx
@@ -0,0 +1,3 @@
+import SeoClusterLanding, { getSeoClusterMetadata } from '@/components/seo/SeoClusterLanding'
+export const metadata = getSeoClusterMetadata('customSoftwareJaipur')
+export default function Page(){ return <SeoClusterLanding pageKey="customSoftwareJaipur" /> }
