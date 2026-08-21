9eb42eece422410642416106a073e75d7a97cb49
Jatin-ge <Jatin@growthescalators.com>
2026-08-21 12:12:04 +0530

    feat: add Jaipur SEO and lead generation landing route

---
+++ app/seo-lead-generation-agency-jaipur/page.tsx
@@ -0,0 +1,6 @@
+import IndustryLandingPage from '@/components/landing/IndustryLandingPage'
+import { SEO_LEAD_GEN_JAIPUR_CONTENT } from './_data/content'
+
+export default function SeoLeadGenerationJaipurPage() {
+  return <IndustryLandingPage content={SEO_LEAD_GEN_JAIPUR_CONTENT} />
+}
