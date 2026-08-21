58a74ce67e8fa68302f4c35ac79964a0df232d73
Jatin-ge <Jatin@growthescalators.com>
2026-08-21 19:58:51 +0530

    Add white-label digital marketing page

---
+++ app/white-label-digital-marketing-agency/page.tsx
@@ -0,0 +1,7 @@
+import WhiteLabelSeoLanding, { getWhiteLabelMetadata } from '@/components/seo/WhiteLabelSeoLanding'
+
+export const metadata = getWhiteLabelMetadata('digitalMarketing')
+
+export default function WhiteLabelDigitalMarketingAgencyPage() {
+  return <WhiteLabelSeoLanding pageKey="digitalMarketing" />
+}
