8ddf299a509a95dc9c452bf87d34c13cde960b59
Jatin-ge <Jatin@growthescalators.com>
2026-08-23 23:22:00 +0530

    Place revenue framework on main D2C page

--- app/d2c/page.tsx
+++ app/d2c/page.tsx
@@ -1,6 +1,7 @@
 import IndustryLandingPage from '@/components/landing/IndustryLandingPage'
+import D2CRevenueSystem from '@/components/landing/D2CRevenueSystem'
 import { D2C_CONTENT } from './_data/content'
 
 export default function D2CPage() {
-  return <IndustryLandingPage content={D2C_CONTENT} />
+  return <IndustryLandingPage content={D2C_CONTENT} growthSystem={<D2CRevenueSystem />} />
 }
