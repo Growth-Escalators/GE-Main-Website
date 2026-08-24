9525218413c9e789e55cfa3d7053f499f4b7e719
Jatin-ge <Jatin@growthescalators.com>
2026-08-24 15:52:45 +0530

    design: migrate travel India to commercial V2

--- app/travel-marketing-agency/page.tsx
+++ app/travel-marketing-agency/page.tsx
@@ -1,8 +1,9 @@
-import PriorityNationalLanding, { getPriorityNationalMetadata } from '@/components/seo/PriorityNationalLanding'
+import PriorityCommercialPage from '@/components/commercial/PriorityCommercialPage'
+import { getPriorityNationalMetadata } from '@/components/seo/PriorityNationalLanding'
 
 const baseMetadata = getPriorityNationalMetadata('travelIndia')
 export const metadata = { ...baseMetadata, title: { absolute: 'Travel Marketing Agency in India | Meta & Google Ads | Growth Escalators' } }
 
 export default function TravelMarketingAgencyPage() {
-  return <PriorityNationalLanding pageKey="travelIndia" />
+  return <PriorityCommercialPage pageKey="travelIndia" />
 }
