c31d95168450050e817f3f9457428ae0cd12627c
Jatin-ge <Jatin@growthescalators.com>
2026-08-24 13:18:01 +0530

    phase 3: upgrade white label digital marketing money page

--- app/white-label-digital-marketing-agency/page.tsx
+++ app/white-label-digital-marketing-agency/page.tsx
@@ -1,8 +1,7 @@
-import WhiteLabelSeoLanding, { WHITE_LABEL_PAGES, getWhiteLabelMetadata } from '@/components/seo/WhiteLabelSeoLanding'
+import WhiteLabelMoneyLanding, { getWhiteLabelMoneyMetadata } from '@/components/seo/WhiteLabelMoneyLanding'
 
-const baseMetadata = getWhiteLabelMetadata('digitalMarketing')
-export const metadata = { ...baseMetadata, title: { absolute: WHITE_LABEL_PAGES.digitalMarketing.title } }
+export const metadata = getWhiteLabelMoneyMetadata('digitalMarketing')
 
 export default function WhiteLabelDigitalMarketingAgencyPage() {
-  return <WhiteLabelSeoLanding pageKey="digitalMarketing" />
+  return <WhiteLabelMoneyLanding pageKey="digitalMarketing" />
 }
